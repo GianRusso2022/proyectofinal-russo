e31cf953d2806cd16a910d5297cb28d20e370da1
GianRusso2022 <patocriv69@gmail.com>
2022-11-26 12:07:20 -0300

    update

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -3,17 +3,22 @@ import { useNavigate } from "react-router-dom";
 import { addOrder } from "../api/orders";
 import { updateManyProducts } from "../api/products";
 import Button from "../components/Button";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import Swal from "sweetalert2";
+
 
 const Cart = () => {
   const navigate = useNavigate()
   const [name, setName] = useState("");
   const [phone, setPhone] = useState("");
   const [email, setEmail] = useState("");
+  
+  
 
   const { getTotal, cart, emptyCart } = useCartContext();
 
   const createOrder = async () => {
+
 
     const items = cart.map(({ id, nombre, qty, precio }) => ({
       id,
@@ -28,14 +33,26 @@ const Cart = () => {
       total: getTotal(),
     };
 
-    const id = await addOrder(order);
-    alert("Pedido realizado, su ID de compra es: " + id)
-    await updateManyProducts(items)
-    emptyCart();
+    if (cart.length === 0) {
+      Swal.fire({
+        icon: 'error',
+        title: 'No hay productos en el carrito',
+        text: 'Para poder realizar un pedido debe agregar productos al carrito',
+      })
+    } else {
+      const id = await addOrder(order);
+      Swal.fire({
+        icon: 'succes',
+        title: 'Pedido realizado',
+        text: `El ID de su compra es ${id}`,
+      })
+      await updateManyProducts(items)
+      emptyCart();
+    }
 
   };
   return (
-    <div className="container">
+    <div className="container w-75 mb-5 mt-5 fix">
       {cart.map((product) => (
         <div
           key={product.id}
@@ -48,9 +65,10 @@ const Cart = () => {
             justifyContent: "space-between",
           }}
         >
-          <div style={{ fontWeight: 600 }}>{product.nombre}</div>
+          <div style={{ width: 200 }}>{product.nombre}</div>
+          
           <div className="container w-50 d-flex justify-content-center">
-            <img src={product.imagen} alt="imagen" width="75px" />
+            <img src={product.imagen} alt="imagen" width="90px" />
           </div>
           <div>Cantidad : {product.qty}</div>
         </div>
@@ -71,19 +89,28 @@ const Cart = () => {
         <input
           style={{ border: "1px solid black", height: 40 }}
           onChange={(e) => setName(e.target.value)}
+          required
+          type="text"
+          placeholder="Introduzca su nombre"
         />
         <span>Telefono</span>
         <input
           style={{ border: "1px solid black", height: 40 }}
           onChange={(e) => setPhone(e.target.value)}
+          required
+          placeholder="Introduzca su telefono de contacto"
         />
         <span>Email</span>
         <input
           style={{ border: "1px solid black", marginBottom: 15, height: 40 }}
           onChange={(e) => setEmail(e.target.value)}
+          required
+          type="email"
+          placeholder="Introduzca su correo electronico"
         />
       </div>
       <Button onClick={createOrder}>Comprar</Button>
+      <Button onClick={emptyCart}>Vaciar carrito</Button>
     </div>
   );
 };
